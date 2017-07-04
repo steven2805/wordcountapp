5ef75902f624b77f9cd79b61756181bcc9558e12
steven2805 <stevenmac90@googlemail.com>
2017-07-04 21:41:36 +0100

    added buttons

--- app/src/main/java/com/example/stevenmacdonald/wordcounter/GetwordActivity.java
+++ app/src/main/java/com/example/stevenmacdonald/wordcounter/GetwordActivity.java
@@ -2,8 +2,16 @@ package com.example.stevenmacdonald.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GetwordActivity extends AppCompatActivity {
+
+    EditText input_test;
+    TextView output_view;
+    Button submitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/stevenmacdonald/wordcounter/WordCountingActivity.java
@@ -0,0 +1,13 @@
+package com.example.stevenmacdonald.wordcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordCountingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_counting);
+    }
+}
